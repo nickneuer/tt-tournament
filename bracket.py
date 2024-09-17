72d12745c58957139c9875756570f99b01256abb
Nick Neuer <nickneuer@Nicks-MacBook-Pro.local>
2024-09-17 09:29:35 -0700

    visualize bracket, update group seeding logic

--- bracket.py
+++ bracket.py
@@ -1,4 +1,5 @@
 import json
+from lib.bracket_viz import Bracket as BracketViz
 
 
 class Player():
@@ -17,13 +18,6 @@ class Group():
 
     def get_max_rating(self):
         return sorted(self.players, key=lambda p: p.rating)[-1].rating
-
-    def __repr__(self):
-        return f'''Group(
-    players={json.dumps([p.__repr__() for p in self.players], indent=4)}, 
-    group_number={self.group_number}
-)
-'''
 
 
 class Bracket():
@@ -58,10 +52,16 @@ class Bracket():
             for i, player in enumerate(last_group):
                 groups[i].append(player)
 
+        # sort players in each group
         groups = [sorted(g, key=lambda p: -1 * p.rating) for g in groups]
-        final_groups = [Group(players=g, group_number=i+1) for i, g in enumerate(groups)]
+        groups = [Group(players=g, group_number=0) for g in groups]
+        # sort groups by seed
+        groups = sorted(groups, key=lambda g: -1 * g.get_max_rating())
+        # update groups number to match seed
+        for i, g in enumerate(groups):
+            g.group_number = i + 1
         #
-        return cls(groups=final_groups)
+        return cls(groups=groups)
 
     def print_groups(self):
         letters = ['A', 'B', 'C', 'D', 'E']
@@ -75,9 +75,29 @@ class Bracket():
                 print(f'{letters[i]}) {player.name} ({player.rating})')
             print()
 
+    def display(self):
+        team_labels = []
+        for group in self.groups:
+            label = f'Group {group.group_number} 1st'
+            team_labels.append(label)
+        for group in self.groups:
+            label = f'Group {group.group_number} 2nd'
+            team_labels.append(label)
+        # instantiate bracket class
+        bviz = BracketViz(team_labels)
+        #bviz.shuffle()
+        bviz.show()
+
+
 
 if __name__ == '__main__':
-    import csv 
+    import csv
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', "--group_size", default=4, type=int)    
+    args = parser.parse_args()
+
 
     players = []
     with open('players.csv') as f:
@@ -85,5 +105,8 @@ if __name__ == '__main__':
         for row in reader:
             players.append(Player(row[0], int(row[1])))
 
-    bracket = Bracket.from_players_list(players, preferred_group_size=3)
+    bracket = Bracket.from_players_list(players, preferred_group_size=args.group_size)
     bracket.print_groups()
+    print()
+    bracket.display()
+    print()
